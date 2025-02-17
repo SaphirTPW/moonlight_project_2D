d2f9eef2dfcc76ebd25e9b6e54e918d12381c8f9
SaphirTPW <nath.bonkena@gmail.com>
2025-02-17 11:15:16 +0100

    Update Script Template and Add PlayerController script

--- Assets/Prototype/_Code/Controllers/PlayerController.cs
+++ Assets/Prototype/_Code/Controllers/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Public Variables 
+    #endregion 
+
+    #region Private Variables 
+    [SerializeField] private float _hInputValue;
+    #endregion
+
+    #region Unity Methods
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckMovementInput();
     }
+    #endregion
+
+    #region Public Methods
+    #endregion 
+
+    #region Private Methods 
+    private void CheckMovementInput()
+    {
+        _hInputValue = Input.GetAxis("Horizontal");
+    }
+    #endregion
+
+    #region Coroutines
+    #endregion
 }
